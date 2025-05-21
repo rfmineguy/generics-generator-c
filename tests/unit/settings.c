d615bb028a0bda81f8437b1997ee1337a6316c0e
Riley Fischer <rfmineguy@gmail.com>
2025-05-21 16:36:10 -0700

    test_settings: Wrote test for custom seting with single path

--- tests/unit/settings.c
+++ tests/unit/settings.c
@@ -9,4 +9,12 @@ static MunitResult test_settings_custom_empty_paths(const MunitParameter params[
 	return MUNIT_OK;
 }
 
+static MunitResult test_settings_custom_with_single_path(const MunitParameter params[], void *userdata) {
+	generator_settings settings = settings_custom(.search_paths=paths("demo_templates"), .outdir=".");
+	munit_assert_int(settings.path_count, ==, 1);
+	munit_assert_string_equal("demo_templates", settings.search_paths[0]);
+	munit_assert_string_equal(".", settings.outdir);
+	return MUNIT_OK;
+}
+
 #endif
