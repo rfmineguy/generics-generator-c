3aadfa7f773c7c56fb4a2742c0badbcf8718e504
Riley Fischer <rfmineguy@gmail.com>
2025-05-21 16:39:03 -0700

    test_replacement: Wrote test for adding new replacement to replacement context

--- tests/unit/replacement.c
+++ tests/unit/replacement.c
@@ -10,4 +10,34 @@ static MunitResult test_replacement_create(const MunitParameter params[], void *
 	replacement_free(&r);
 	return MUNIT_OK;
 }
+
+static MunitResult test_replacement_add(const MunitParameter params[], void *userdata) {
+	// 0. Setup a raw form of replacements
+	//    [0] -> needle
+	//    [1] -> with
+	const char* replacements[4][2] = {
+		{"needle", "with"},
+		{"$T",     "int"},
+		{"^T",     "int"},
+		{"pod",    "something else"},
+	};
+
+	// 1. Create the replacement context (tested by 'test_replacement_create')
+	replacement r = replacement_create();
+
+	// 2. Setup initial replacement set using the 'replacement_add' (currently being tested)
+	for (int i = 0; i < 4; i++)	
+		replacement_add(&r, replacements[i][0], replacements[i][1]);
+
+	// 3. Check to make sure that the content in the replacement context matches the data set
+	//    it was created from
+	munit_assert_int(r.replacements_count, ==, 4);
+	for (int i = 0; i < 4; i++) {
+		munit_assert_string_equal(r.replacements[i].needle, replacements[i][0]);
+		munit_assert_string_equal(r.replacements[i].with,   replacements[i][1]);
+	}
+
+	replacement_free(&r);
+	return MUNIT_OK;
+}
 #endif
