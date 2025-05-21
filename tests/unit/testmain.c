c329c2b1eb32814b5c8efd364453f7232e74d1ff
Riley Fischer <rfmineguy@gmail.com>
2025-05-21 16:43:04 -0700

    Unit: Setup main unit testing entry point

---
+++ tests/unit/testmain.c
@@ -0,0 +1,16 @@
+#include "alltests.h"
+
+MunitSuite suite[] = {
+	{"/settings",    settings_tests,    NULL, 1, MUNIT_SUITE_OPTION_NONE},
+	{"/replacement", replacement_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
+	{"/forward_table", forward_table_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
+	{NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE},
+};
+
+static const MunitSuite main_suite = {
+	"", NULL, suite, 1, MUNIT_SUITE_OPTION_NONE
+};
+
+int main(int argc, char** argv) {
+	return munit_suite_main(&main_suite, NULL, argc, argv);
+}
