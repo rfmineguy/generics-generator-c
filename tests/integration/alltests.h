795430aeb7eb67cc8af5aac9bd8e473fd0aee3d3
Riley Fischer <rfmineguy@gmail.com>
2025-05-21 16:48:47 -0700

    Alltests: Add file similar to the unit tests where all the integration test prototypes go

---
+++ tests/integration/alltests.h
@@ -0,0 +1,9 @@
+#ifndef ALLTESTS_H
+#define ALLTESTS_H
+#include "../lib/munit.h"
+#include "../lib/munit_ext.h"
+
+static MunitResult test_generator_linkedlist_int(const MunitParameter params[], void *userdata);
+extern MunitTest generator_tests[];
+
+#endif
