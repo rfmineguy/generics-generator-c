7a35a634e632517e76c33e3dda420d4ae6e6edf8
Riley Fischer <rfmineguy@gmail.com>
2025-05-22 23:04:24 -0700

    testlib: implemented new assertion for checking if a replacement contains a needle/with pair

--- tests/lib/munit_ext.h
+++ tests/lib/munit_ext.h
@@ -12,5 +12,23 @@
 	} while (0)
   MUNIT_POP_DISABLE_MSVC_C4127_
 
+#define munit_assert_replacement_contains(replacement, needle_, with_)\
+	do {\
+		int i = 0;\
+		for (i = 0; i < replacement.replacements_count; i++) {\
+			const char* r_needle = replacement.replacements[i].needle;\
+			const char* r_with = replacement.replacements[i].with;\
+			int needle_cmp = (needle_ == NULL && r_needle == NULL) || (needle_ && r_needle && strcmp(needle_, r_needle) == 0);\
+			int with_cmp = (with_ == NULL && r_with == NULL) || (with_ && r_with && strcmp(with_, r_with) == 0);\
+			if (needle_cmp && with_cmp) {\
+				break;\
+			}\
+		}\
+		if (i == replacement.replacements_count) {\
+			munit_errorf("assertion failed: does not contain { needle: %s, with: %s }", (char*)needle_, (char*)with_);\
+		}\
+    MUNIT_PUSH_DISABLE_MSVC_C4127_ \
+	} while (0)
+  MUNIT_POP_DISABLE_MSVC_C4127_
 
 #endif
