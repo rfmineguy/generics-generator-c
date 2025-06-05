d5efd29aae1fc8f6534831d24a309d7032fa0e37
Riley Fischer <rfmineguy@gmail.com>
2025-06-04 20:15:16 -0700

    demo: adjust demo to utilize new embed feature and formatted forward items

--- demo.c
+++ demo.c
@@ -1,8 +1,8 @@
 #define GENGEN_IMPLEMENTATION
-#include "gengen.h"
+#include "gengen2.h"
 
 int main() {
-	ctemplate linkedlist = template_create();
+	ctemplate linkedlist = template_create("linkedlist");
 	template_addfile       (&linkedlist, "linked_list.htpl", "linked_list_$T.h");
 	template_addfile       (&linkedlist, "linked_list.ctpl", "linked_list_$T.c");
 	template_addreplacement(&linkedlist, "$T",     NULL);
@@ -11,10 +11,11 @@ int main() {
 	template_addreplacement(&linkedlist, "FREE",   "free");
 	template_addreplacement(&linkedlist, "CALLOC", "calloc");
 	template_addreplacement(&linkedlist, "HEADER", "stdint.h");
+	template_addreplacement(&linkedlist, "FDECL",  "stdint.h");
 
-	ctemplate binarytree = template_create();
-	template_addfile    	 (&binarytree, "binary_tree.htpl", "binary_tree_^KEY_^VAL.h");
-	template_addfile    	 (&binarytree, "binary_tree.ctpl", "binary_tree_^KEY_^VAL.c");
+	ctemplate binarytree = template_create("binarytree");
+	template_addfile    	 (&binarytree, "bst.htpl", "bst_$KEY_$VAL.h");
+	template_addfile    	 (&binarytree, "bst.ctpl", "bst_$KEY_$VAL.c");
 	template_addreplacement(&binarytree, "$KEY",   NULL);
 	template_addreplacement(&binarytree, "^KEY",   NULL);
 	template_addreplacement(&binarytree, "$VAL",   NULL);
@@ -24,7 +25,7 @@ int main() {
 	template_addreplacement(&binarytree, "CALLOC", "calloc");
 	template_addreplacement(&binarytree, "HEADER", "stdint.h");
 
-	ctemplate queue  	  =  template_create();
+	ctemplate queue  	  =  template_create("queue");
 	template_addfile       (&queue, "queue.htpl", "queue_$T.h");
 	template_addfile       (&queue, "queue.ctpl", "queue_$T.c");
 	template_addreplacement(&queue, "$T",     NULL);
@@ -35,12 +36,14 @@ int main() {
 	template_addreplacement(&queue, "HEADER", "stdint.h");
 
 	forward_table fwd_q_ll = forward_table_create();
-	forward_table_forward(&fwd_q_ll, fwd(.symbol="$T", .as="$T"));
-	forward_table_forward(&fwd_q_ll, fwd(.symbol="^T", .as="^T"));
-	template_adddep(&queue, linkedlist, fwd_q_ll);
+	forward_table_forward(&fwd_q_ll, fwd(symbollit("$T"), .as="$T"));
+	forward_table_forward(&fwd_q_ll, fwd(symbollit("^T"), .as="^T"));
+	template_adddep(&queue, linkedlist, fwd_q_ll, dep_settings());
 
-	forward_table fwd_q_bt = forward_table_create();
-	forward_table_forward(&fwd_q_bt, fwd(.symbol="$T", .as="$T"));
+	forward_table fwd_bst_ll = forward_table_create();
+	forward_table_forward(&fwd_bst_ll, fwd(symbolfmt("{$KEY}_{$VAL}"), .as="$T"));
+	forward_table_forward(&fwd_bst_ll, fwd(symbolfmt("bst_data_node_{$KEY}_{$VAL}"), .as="^T"));
+	template_adddep(&binarytree, linkedlist, fwd_bst_ll, dep_settings(.embed=true, .embedloc=0));
 
 	replacement ll_int = replacement_create();
 	replacement_add(&ll_int, "$T",     "int");
@@ -50,11 +53,11 @@ int main() {
 	replacement_add(&ll_string, "$T",  "string");
 	replacement_add(&ll_string, "^T",  "const char*");
 
-	replacement bt_int = replacement_create();
-	replacement_add(&bt_int, "$KEY",   "const char*");
-	replacement_add(&bt_int, "^KEY",   "string");
-	replacement_add(&bt_int, "$VAL",	 "int");
-	replacement_add(&bt_int, "^VAL", 	 "int");
+	replacement bst_string_int = replacement_create();
+	replacement_add(&bst_string_int, "$KEY",   "string");
+	replacement_add(&bst_string_int, "^KEY",   "const char*");
+	replacement_add(&bst_string_int, "$VAL",	 "int");
+	replacement_add(&bst_string_int, "^VAL", 	 "int");
 
 	replacement queue_int = replacement_create();
 	replacement_add(&queue_int, "$T",  "int");
@@ -64,17 +67,21 @@ int main() {
 	replacement_add(&queue_long, "$T",  "long");
 	replacement_add(&queue_long, "^T",  "long");
 
-	generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), linkedlist, ll_int);
-	generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), linkedlist, ll_string);
-	generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), queue     , queue_int);
-	generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), queue     , queue_long);
+	// generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), linkedlist, ll_int);
+	// generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), linkedlist, ll_string);
+	// generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), queue     , queue_int);
+	// generator_run(settings_custom(.search_paths=paths("demo_templates", "."), .outdir="."), queue     , queue_long);
+	generator_run2(gen_settings(.embed_deps = true, .verbose = false, .dryrun = true, .search_paths=paths("demo_templates", "."), .outdir="output"), binarytree, bst_string_int);
 
 	template_free(&linkedlist);
 	template_free(&binarytree);
 	template_free(&queue);
 	replacement_free(&ll_int);
 	replacement_free(&ll_string);
-	replacement_free(&bt_int);
+	replacement_free(&bst_string_int);
 	replacement_free(&queue_int);
+	replacement_free(&queue_long);
+	forward_table_free(&fwd_q_ll);
+	forward_table_free(&fwd_bst_ll);
 }
 
